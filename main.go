d1ea147612a0f9e5c3c11c784435ff7e91423c67
Konstantin <bolotink@ispras.ru>
2025-03-23 20:28:52 +0300

    rels#2402: Added qos and accounting info

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ import (
 
 func init() {
 	// Metrics have to be registered to be exposed
+	prometheus.MustRegister(NewAssocCollector())
 	prometheus.MustRegister(NewPrioCollector())
 	prometheus.MustRegister(NewJobCollector())
 	prometheus.MustRegister(NewNodeResCollector())
