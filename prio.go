df6808580a1d3e04894c0ac44aa69f3b7bee091a
Konstantin <bolotink@ispras.ru>
2025-04-17 15:21:25 +0300

    rels #2404: Added metric that collects priority configuration

--- prio.go
+++ prio.go
@@ -25,13 +25,33 @@ type PrioMetrics struct {
 	user             string
 }
 
-func PrioGetMetrics() map[string]*PrioMetrics {
+type PriorityConfigs struct {
+	PriorityParameters           string
+	PrioritySiteFactorParameters string
+	PrioritySiteFactorPlugin     string
+	PriorityDecayHalfLife        string
+	PriorityCalcPeriod           string
+	PriorityFavorSmall           string
+	PriorityFlags                string
+	PriorityMaxAge               string
+	PriorityUsageResetPeriod     string
+	PriorityType                 string
+	PriorityWeightAge            string
+	PriorityWeightAssoc          string
+	PriorityWeightFairShare      string
+	PriorityWeightJobSize        string
+	PriorityWeightPartition      string
+	PriorityWeightQOS            string
+	PriorityWeightTRES           string
+}
+
+func PrioGetMetrics() (map[string]*PrioMetrics, PriorityConfigs) {
 	return ParsePrioMetrics(PrioData())
 }
 
 // ParseNodeMetrics takes the output of sinfo with node data
 // It returns a map of metrics per node
-func ParsePrioMetrics(input []byte) map[string]*PrioMetrics {
+func ParsePrioMetrics(input []byte) (map[string]*PrioMetrics, PriorityConfigs) {
 	priorities := make(map[string]*PrioMetrics)
 	lines := strings.Split(string(input), "\n")
 
@@ -61,8 +81,86 @@ func ParsePrioMetrics(input []byte) map[string]*PrioMetrics {
 
 		}
 	}
+	config := PriorityConfigs{
+		PriorityParameters:           "",
+		PrioritySiteFactorParameters: "",
+		PrioritySiteFactorPlugin:     "",
+		PriorityDecayHalfLife:        "",
+		PriorityCalcPeriod:           "",
+		PriorityFavorSmall:           "",
+		PriorityFlags:                "",
+		PriorityMaxAge:               "",
+		PriorityUsageResetPeriod:     "",
+		PriorityType:                 "",
+		PriorityWeightAge:            "",
+		PriorityWeightAssoc:          "",
+		PriorityWeightFairShare:      "",
+		PriorityWeightJobSize:        "",
+		PriorityWeightPartition:      "",
+		PriorityWeightQOS:            "",
+		PriorityWeightTRES:           "",
+	}
+	lines = strings.Split(string(PrioConfig()), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "PriorityWeightTRES") {
+			config.PriorityWeightTRES = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityParameters") {
+			config.PriorityParameters = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PrioritySiteFactorParameters") {
+			config.PrioritySiteFactorParameters = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PrioritySiteFactorPlugin") {
+			config.PrioritySiteFactorPlugin = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityDecayHalfLife") {
+			config.PriorityDecayHalfLife = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityCalcPeriod") {
+			config.PriorityCalcPeriod = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityFavorSmall") {
+			config.PriorityFavorSmall = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityFlags") {
+			if len(strings.Fields(line)) == 2 {
+				config.PriorityFlags = "(null)"
+			} else {
+				config.PriorityFlags = strings.Fields(line)[2]
+			}
+		}
+		if strings.HasPrefix(line, "PriorityMaxAge") {
+			config.PriorityMaxAge = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityUsageResetPeriod") {
+			config.PriorityUsageResetPeriod = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityType") {
+			config.PriorityType = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightAge") {
+			config.PriorityWeightAge = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightAssoc") {
+			config.PriorityWeightAssoc = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightFairShare") {
+			config.PriorityWeightFairShare = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightJobSize") {
+			config.PriorityWeightJobSize = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightPartition") {
+			config.PriorityWeightPartition = strings.Fields(line)[2]
+		}
+		if strings.HasPrefix(line, "PriorityWeightQOS") {
+			config.PriorityWeightQOS = strings.Fields(line)[2]
+		}
 
-	return priorities
+	}
+
+	return priorities, config
 }
 
 // NodeData executes the sinfo command to get data for each node
@@ -76,8 +174,18 @@ func PrioData() []byte {
 	return out
 }
 
+func PrioConfig() []byte {
+	cmd := exec.Command("scontrol", "show", "conf")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return out
+}
+
 type PrioCollector struct {
-	prio *prometheus.Desc
+	prio     *prometheus.Desc
+	prioconf *prometheus.Desc
 }
 
 // NewNodeCollector creates a Prometheus collector to keep all our stats in
@@ -85,19 +193,25 @@ type PrioCollector struct {
 func NewPrioCollector() *PrioCollector {
 	labels := []string{"JOBID", "PRIORITY", "AGE_FACT", "ASSOC_FACT", "PARTITION_FACT", "JOBSIZE_FACT", "QOS", "NICE_FACT", "ACCOUNT", "QOS_FACT", "PARTITION", "TRES_FACT", "USER"}
 
+	conf_labels := []string{"PriorityParameters", "PrioritySiteFactorParameters", "PrioritySiteFactorPlugin", "PriorityDecayHalfLife", "PriorityCalcPeriod", "PriorityFavorSmall", "PriorityFlags", "PriorityMaxAge", "PriorityUsageResetPeriod", "PriorityType", "PriorityWeightAge", "PriorityWeightAssoc", "PriorityWeightFairShare", "PriorityWeightJobSize", "PriorityWeightPartition", "PriorityWeightQOS", "PriorityWeightTRES"}
+
 	return &PrioCollector{
 		prio: prometheus.NewDesc("slurm_prio", "JOB's priority", labels, nil),
+
+		prioconf: prometheus.NewDesc("slurm_prio_conf", "SLurm Priority Configuration", conf_labels, nil),
 	}
+
 }
 
-// Send all metric descriptions
+// Send all metric Descriptions
 func (nc *PrioCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- nc.prio
 }
 
 func (nc *PrioCollector) Collect(ch chan<- prometheus.Metric) {
-	priorities := PrioGetMetrics()
+	priorities, conf := PrioGetMetrics()
 	for job := range priorities {
 		ch <- prometheus.MustNewConstMetric(nc.prio, prometheus.GaugeValue, float64(0), job, priorities[job].priority, priorities[job].age_factor, priorities[job].assoc_factor, priorities[job].partition_factor, priorities[job].jobsize_factor, priorities[job].qos_name, priorities[job].nice_factor, priorities[job].account, priorities[job].qos_factor, priorities[job].partition, priorities[job].tres_factor, priorities[job].user)
 	}
+	ch <- prometheus.MustNewConstMetric(nc.prioconf, prometheus.GaugeValue, float64(0), conf.PriorityParameters, conf.PrioritySiteFactorParameters, conf.PrioritySiteFactorPlugin, conf.PriorityDecayHalfLife, conf.PriorityCalcPeriod, conf.PriorityFavorSmall, conf.PriorityFlags, conf.PriorityMaxAge, conf.PriorityUsageResetPeriod, conf.PriorityType, conf.PriorityWeightAge, conf.PriorityWeightAssoc, conf.PriorityWeightFairShare, conf.PriorityWeightJobSize, conf.PriorityWeightPartition, conf.PriorityWeightQOS, conf.PriorityWeightTRES)
 }
